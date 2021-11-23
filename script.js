3f8c1403385335d948e85f588b7d52af09d1474d
Kaiomax <94923428+kaiomaxnascimento@users.noreply.github.com>
2021-11-23 16:30:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const fields = document.querySelectorAll('[required]')
+
+function ValidateField(field) {
+    function verifyErrors() {
+        let foundError = false
+        for (let chaveErro in field.validity) {
+            if (field.validity[chaveErro] && !field.validity.valid) {
+                foundError = chaveErro
+            }
+        }
+        return foundError
+    }
+    console.log(field.validity)
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: 'Por favor, preencha este campo'
+            },
+            email: {
+                valueMissing: 'Email é obrigatório',
+                typeMismatch: 'Por favor, preencha um email válido'
+            },
+            password: {
+                valueMissing: 'Digite uma Senha',
+                tooShort: 'A senha deve conter pelo menos 6 caracteres'
+
+            }
+        }
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector('span.error')
+        if (message) {
+            spanError.classList.add('active')
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove('active')
+            spanError.innerHTML = ''
+        }
+    }
+
+    return function () {
+
+        const error = verifyErrors()
+
+        if (error) {
+            const message = customMessage(error)
+            field.style.borderColor = 'red'
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = 'green'
+            setCustomMessage()
+        }
+    }
+}
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for (let field of fields) {
+    field.addEventListener('invalid', event => {
+
+        event.preventDefault()
+        customValidation(event)
+
+    })
+    field.addEventListener('blur', customValidation)
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    console.log('enviar o formulário')
+    //preventDefault vai cancelar o evento
+    event.preventDefault()
+})
+
